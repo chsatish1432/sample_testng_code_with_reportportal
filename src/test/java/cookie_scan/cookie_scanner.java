3ed69c1ef1902ac43f06b02466809f219b48b691
chintalapudi.satish <chintalapudi.satish@accenture.com>
2020-09-11 15:51:24 +0530

    first commit

---
+++ src/test/java/cookie_scan/cookie_scanner.java
@@ -0,0 +1,39 @@
+package cookie_scan;
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+
+
+public class cookie_scanner extends testBase{
+
+	
+	
+	
+	@Test(priority=1)
+	public void test1(){
+		System.out.println("Test1 Passed!");
+		
+		
+	}
+	
+	@Test(priority=2)
+	public void test2(){
+		
+		System.out.println("Test2 Passed!");
+		
+	}
+	
+	@Test(priority=3)
+	public void test3(){
+		
+		System.out.println("Test3 Passed!");
+		
+	}
+	
+	
+}
